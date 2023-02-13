c10f28f144d7708d281b48d428ddd3eb84916058
Apratim <apratim.ankur@gmail.com>
2023-02-13 17:03:05 +0530

    added host & frequency configs, main - rects & cleanup (wip - working state)

--- src/component/main.js
+++ src/component/main.js
@@ -17,6 +17,8 @@ const fs = require('fs');
 // Configuration
 const PORT = 3000;
 const HOST = "localhost";
+
+const TUNNEL_HOST = config.TUNNEL_HOST;
 const TERMINAL_SERVICE_URL = config.TERMINAL_SERVICE_URL;
 const REQUEST_HANDLER_URL = config.REQUEST_HANDLER_URL
 let tunnel;
@@ -115,82 +117,90 @@ app.post('/api/network_config', function (req, res) {
     res.status(200).send();
 });
 
-async function createTunnel(port, subdomain, host = 'https://localtunnel.vyom.cc') {
-    const tunnel = await localtunnel({
-        port: port,
-        host: host,
-        subdomain: subdomain.trim()
-    }).catch(err => {
-        throw err;
-    });
+function createTunnel(port, subdomain, host=TUNNEL_HOST, _timeout=5000) {
 
-    tunnel.on('error', err => {
-        console.error("Error Occurred in creating tunnel ", err);
-        clearInterval(refreshIntervalId);
-        syncSystemInformationToServer({deviceStatus: 'offline'});
-        throw err;
-    });
+    console.debug("DBEUG: Create Tunnel Request for - ", subdomain, port, host);
+    return new Promise(async (resolve, reject)=>{
+        try{
+            setTimeout(() => {
+              return reject(Error(`ERROR: Tunnel Creation timed out after %{_timeout/1000}s.`))
+            }, _timeout);
 
-    tunnel.on('request', (info) => {
-        console.log("Tunnel Request Received: ", info);
-    })
-
-    console.log('your url is: %s', tunnel.url);
-    try {
-        await syncSystemInformationToServer({tunnelUrl: tunnel.url, deviceStatus: 'online'});
-    } catch (e) {
-        console.error(e);
-    }
-    /**
-     * `cachedUrl` is set when using a proxy server that support resource caching.
-     * This URL generally remains available after the tunnel itself has closed.
-     * @see https://github.com/localtunnel/localtunnel/pull/319#discussion_r319846289
-     */
-    if (tunnel.cachedUrl) {
-        console.log('your cachedUrl is: %s', tunnel.cachedUrl);
-    }
-}
-
-async function setupTunnel({port, clientId}) {
-    console.log("Starting Tunnel...", port, clientId);
-    try {
-        tunnel = await localtunnel({port: port, subdomain: clientId.trim(), host: 'https://localtunnel.vyom.cc'});
-    } catch (e) {
-        console.error("Error:", e);
-        throw e;
-    }
-
-    // the assigned public url for your tunnel
-    // i.e. https://abcdefgjhij.localtunnel.vyom.cc
-    console.log(`Tunnel URL: ${tunnel.url}`);
-    await syncSystemInformationToServer({tunnelUrl: tunnel.url, deviceStatus: 'online'});
-    tunnel.on('close', () => {
-        // tunnels are closed
-        console.error("Tunnel closed unexpectedly");
-        clearInterval(refreshIntervalId);
-        syncSystemInformationToServer({deviceStatus: 'offline'});
-    });
-    tunnel.on('error', (err) => {
-        // handle errors
-        console.error("Error Occurred in creating tunnel ", err);
-        clearInterval(refreshIntervalId);
-        syncSystemInformationToServer({deviceStatus: 'offline'});
-    });
-    tunnel.on('request', (info) => {
-        if (info.path !== '/cli/') {
-            console.log("Tunnel Request Received: ", info);
+            var tunnel = await localtunnel({
+                port: port,
+                host: host,
+                subdomain: subdomain.trim()
+            });
+        }catch(err){
+            return reject(err);
         }
+
+        tunnel.on('error', err => {
+            console.error("Error Occurred in creating tunnel ", err);
+            syncSystemInfoWithHost({deviceStatus: 'offline'});
+            return reject(err);
+        });
+
+        tunnel.on('request', (info) => {
+            console.log("Tunnel Request Received: ", info);
+        })
+
+        /**
+         * `cachedUrl` is set when using a proxy server that support resource caching.
+         * This URL generally remains available after the tunnel itself has closed.
+         * @see https://github.com/localtunnel/localtunnel/pull/319#discussion_r319846289
+         */
+        if (tunnel.cachedUrl) {
+            console.debug('DBEUG Tunnel Setup with cachedUrl : %s', tunnel.cachedUrl);
+        }
+
+        console.debug('DBEUG: Tunnel Setup at : %s', tunnel.url);
+
+        return resolve(tunnel);
     })
 }
+
+// async function setupTunnel({port, clientId}) {
+//     console.log("Starting Tunnel...", port, clientId);
+//     try {
+//         tunnel = await localtunnel({port: port, subdomain: clientId.trim(), host: 'https://localtunnel.vyom.cc'});
+//     } catch (e) {
+//         console.error("Error:", e);
+//         throw e;
+//     }
+
+//     // the assigned public url for your tunnel
+//     // i.e. https://abcdefgjhij.localtunnel.vyom.cc
+//     console.log(`Tunnel URL: ${tunnel.url}`);
+//     await syncSystemInfoWithHost({tunnelUrl: tunnel.url, deviceStatus: 'online'});
+//     tunnel.on('close', () => {
+//         // tunnels are closed
+//         console.error("Tunnel closed unexpectedly");
+//         clearInterval(refreshIntervalId);
+//         syncSystemInfoWithHost({deviceStatus: 'offline'});
+//     });
+//     tunnel.on('error', (err) => {
+//         // handle errors
+//         console.error("Error Occurred in creating tunnel ", err);
+//         clearInterval(refreshIntervalId);
+//         syncSystemInfoWithHost({deviceStatus: 'offline'});
+//     });
+//     tunnel.on('request', (info) => {
+//         if (info.path !== '/cli/') {
+//             console.log("Tunnel Request Received: ", info);
+//         }
+//     })
+// }
 
 async function getSystemInformation() {
     //console.log(currentSystemInfo);
     return await si.getAllData('*', '*');
 }
 
-async function syncSystemInformationToServer(additionalInfo) {
-    console.log("Syncing System Information to Server...", additionalInfo);
-    var data = {...await getSystemInformation(), ...additionalInfo};
+
+async function pingSystemInfoToHost(_info) {
+    console.log("Syncing System Information to Server...", _info);
+    var data = {...await getSystemInformation(), ..._info};
     try {
         const response = await fetch(config.PLATFORM_BACKEND_URL + `api/client/${config.TUNNEL_CLIENT_ID}`, {
             method: 'PUT',
@@ -201,11 +211,32 @@ async function syncSystemInformationToServer(additionalInfo) {
             body: JSON.stringify(data),
             redirect: 'follow'
         })
-        console.log(`${new Date()} INFO: syncSystemInformationToServer API response status`, response.status)
+        console.log(`${new Date()} INFO: pingSystemInfoToHost API response status`, response.status)
         return response;
     } catch (e) {
         throw e;
     }
+}
+
+async function syncSystemInfoWithHost(_info, _frequency=config.SYNC_FREQUENCY) {
+
+    console.debug("DEBUG: Starting Device Info Sync at frequency = ", _frequency, ". Info = ", _info);
+
+    if(_info.deviceStatus == "offline"){
+        return clearInterval(refreshIntervalId);
+    }
+    if(refreshIntervalId){
+        console.debug("DEBUG: syncSystemInfoWithHost Request Received but it is already running");
+        return;
+    }
+
+    refreshIntervalId = setInterval(async () => {
+        try {
+            await pingSystemInfoToHost(_info);
+        } catch (e) {
+            console.log(e);
+        }
+    }, _frequency);   
 }
 
 function sleep(ms) {
@@ -214,21 +245,23 @@ function sleep(ms) {
 
 app.__start__ = async (cb) => {
     try {
-        kill(PORT).then(() => {
-            const server = app.listen(PORT, HOST, async () => {
-                console.log(`Starting Proxy at ${HOST}:${PORT}`);
-                await sleep(5000)
-                await createTunnel(PORT, `${config.TUNNEL_CLIENT_ID}`);
-                refreshIntervalId = setInterval(async () => {
-                    try {
-                        await syncSystemInformationToServer();
-                    } catch (e) {
-                        console.log(e);
-                    }
-                }, 60000);
-                var dynamicController = require('./controllers/RuntimeController');
-                dynamicController.init(app);
-            });
+        kill(PORT).then(async () => {
+            const server = await app.listen(PORT, HOST);
+
+            console.log(`Started Proxy at ${HOST}:${PORT}`);
+            // await sleep(5000)
+
+            try{
+                var tunnel = await createTunnel(PORT, `${config.TUNNEL_CLIENT_ID}`);
+            }catch(e){
+                throw e;
+            }
+
+            syncSystemInfoWithHost({tunnelUrl: tunnel.url, deviceStatus: 'online'})
+            
+            var dynamicController = require('./controllers/RuntimeController');
+            dynamicController.init(app);
+
             server.on('upgrade', wsProxy.upgrade);
         });
     } catch (e) {
--- src/configs/config.js
+++ src/configs/config.js
@@ -1,15 +1,19 @@
 var devConfig = {
     PLATFORM_BACKEND_URL: "http://localhost:8888/",
+    TUNNEL_HOST: "http://localhost:3344",
     TUNNEL_CLIENT_ID: process.env.CLIENT_ID,
     TERMINAL_SERVICE_URL: process.env.TERMINAL_SERVICE_URL,
-    REQUEST_HANDLER_URL: process.env.REQUEST_HANDLER_URL
+    REQUEST_HANDLER_URL: process.env.REQUEST_HANDLER_URL,
+    SYNC_FREQUENCY: 10000
 }
 
 var prodConfig = {
     PLATFORM_BACKEND_URL: "https://teleport.vyom.cc/",
+    TUNNEL_HOST: "https://localtunnel.vyom.cc",
     TUNNEL_CLIENT_ID: process.env.CLIENT_ID,
     TERMINAL_SERVICE_URL: process.env.TERMINAL_SERVICE_URL,
-    REQUEST_HANDLER_URL: process.env.REQUEST_HANDLER_URL
+    REQUEST_HANDLER_URL: process.env.REQUEST_HANDLER_URL,
+    SYNC_FREQUENCY: 10000
 }
 
 var config = process.env.NODE_ENV === 'development' ? devConfig : prodConfig
